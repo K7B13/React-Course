a0016b233357089f393c2928aa6c479b74b1c764
Darko Dimkov <darko@thrasker.com>
2023-11-11 19:18:03 +0100

    working on ErrorBoundery

---
+++ adopt-me/src/ErrorBoundary.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+
+class ErrorBoundary extends Component {
+    state = { hasError: false };
+    static getDerivedStateFromError() {
+        return { hasError: ture }
+    }
+}
+
+ErrorBoundary.getDerivedStateFromError();
