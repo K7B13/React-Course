cdf8e714d38d711198e2b36dea0aade5972023a4
Darko Dimkov <darko@thrasker.com>
2023-12-08 00:56:08 +0100

    still working on ErrorBoundary

--- adopt-me/src/ErrorBoundary.jsx
+++ adopt-me/src/ErrorBoundary.jsx
@@ -5,8 +5,11 @@ import { Link } from "react-router-dom";
 class ErrorBoundary extends Component {
     state = { hasError: false };
     static getDerivedStateFromError() {
-        return { hasError: ture }
+        return { hasError: true }
+    }
+
+    componentDidCatch(error, info) {
+        console.error("ErrorBoundary component caught an error", error, info)
     }
 }
 
-ErrorBoundary.getDerivedStateFromError();
